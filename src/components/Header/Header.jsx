7e7bfcd228505bc85fe4c6ece80768703b2aa648
Maz Radwan <maz.radwan@keyin.com>
2024-08-01 21:41:25 -0230

    hamburger menu for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 import Button from "../Button/Button";
-import logo from "../../assets/lyonsdenlogo.png"; // Ensure this path is correct
+import logo from "../../assets/lyonsdenlogo.png";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={styles.navBar}>
       <a href="/" className={styles.logoContainer}>
@@ -14,24 +20,35 @@ const Header = () => {
           <span className={styles.siteSubName}>THERAPY</span>
         </div>
       </a>
-      <ul className={styles.navLinks}>
-        <li>
-          <a href="/about" className={styles.navLink}>
-            About
-          </a>
-        </li>
-        <li>
-          <a href="/services" className={styles.navLink}>
-            Therapy Services
-          </a>
-        </li>
-        <li>
-          <a href="/contact" className={styles.navLink}>
-            Contact
-          </a>
-        </li>
-      </ul>
-      <Button className={styles.appointmentButton}>Book an Appointment</Button>
+      <div
+        className={`${styles.navContainer} ${isMenuOpen ? styles.open : ""}`}
+      >
+        <ul className={styles.navLinks}>
+          <li>
+            <a href="/about" className={styles.navLink}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="/services" className={styles.navLink}>
+              Therapy Services
+            </a>
+          </li>
+          <li>
+            <a href="/contact" className={styles.navLink}>
+              Contact
+            </a>
+          </li>
+        </ul>
+        <Button className={styles.appointmentButton}>
+          Book an Appointment
+        </Button>
+      </div>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   );
 };
